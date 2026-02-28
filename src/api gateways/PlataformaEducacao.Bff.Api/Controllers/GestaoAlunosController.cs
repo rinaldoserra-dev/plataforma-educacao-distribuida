37bb54665aecdf79cffcd7ca94ebdf8cd28958b0
Marcio <marcio28@gmail.com>
2026-02-28 13:27:41 -0300

    Implementando BFF de Alunos API.

---
+++ src/api gateways/PlataformaEducacao.Bff.Api/Controllers/GestaoAlunosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PlataformaEducacao.Bff.Api.Services;
+
+namespace PlataformaEducacao.Bff.Api.Controllers
+{
+    [Route("alunos")]
+    [Authorize]
+    public class GestaoAlunosController : BaseController
+    {
+        private readonly IAlunosService _alunosService;
+
+        public GestaoAlunosController(IAlunosService alunosService)
+        {
+            _alunosService = alunosService;
+        }
+
+
+
+    }
+}
---
+++ src/api gateways/PlataformaEducacao.Bff.Api/Services/AlunosService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using PlataformaEducacao.Bff.Api.Extensions;
+using PlataformaEducacao.Core.Communication;
+
+namespace PlataformaEducacao.Bff.Api.Services
+{
+    public interface IAlunosService
+    {
+        Task<ResponseResult> Matricular(MatricularDTO matricular);
+
+    }
+    public class AlunosService : Service, IAlunosService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AlunosService(HttpClient httpClient, IOptions<AppServicesSettings> settings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(settings.Value.GestaoConteudoUrl);
+        }
+
+        public async Task<ResponseResult> Matricular(MatricularDTO matricular)
+        {
+        }
+    }
+}
