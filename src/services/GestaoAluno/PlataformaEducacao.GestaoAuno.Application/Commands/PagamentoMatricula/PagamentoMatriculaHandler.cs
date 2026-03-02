e262683110987953132c51c7520b1afe9edf26ea
Márcio Gomes Gonçalves <gomes@metha.com.br>
2026-03-01 21:33:34 -0300

    Inclusão do parâmentro tipoDePagamento 1 (boleto) na criação do evento IniciaPagamentoIntegrationEvent.

--- src/services/GestaoAluno/PlataformaEducacao.GestaoAuno.Application/Commands/PagamentoMatricula/PagamentoMatriculaHandler.cs
+++ src/services/GestaoAluno/PlataformaEducacao.GestaoAuno.Application/Commands/PagamentoMatricula/PagamentoMatriculaHandler.cs
@@ -55,7 +55,11 @@ namespace PlataformaEducacao.GestaoAluno.Application.Commands.PagamentoMatricula
 
             aluno.RealizarPagamentoMatricula(matricula);
 
-            var pagamentoIniciado = new IniciaPagamentoIntegrationEvent(matricula.Id, matricula.CursoId, matricula.AlunoId, message.Total, message.NomeCartao, message.NumeroCartao, message.ExpiracaoCartao, message.CvvCartao);
+            int pagamentoPorBoleto = 1;
+
+            var pagamentoIniciado = new IniciaPagamentoIntegrationEvent(matricula.Id, matricula.CursoId, 
+                matricula.AlunoId, message.Total, pagamentoPorBoleto, message.NomeCartao, message.NumeroCartao, 
+                message.ExpiracaoCartao, message.CvvCartao);
 
             await _bus.PublishAsync(pagamentoIniciado);
 
